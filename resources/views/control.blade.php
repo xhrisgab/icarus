0659b7ab7a77bce4c18116cd70b168e5374e75fc
xhrisgab <xhris.gab@gmail.com>
2025-02-12 12:24:04 -0400

    Modificacion segun colores

--- resources/views/control.blade.php
+++ resources/views/control.blade.php
@@ -2,17 +2,17 @@
 
 @section('content')
     {{-- Body Control --}}
-    <div class="container mt-4">
+    <div class="container mt-4" style="background-color:rgb(5, 25, 38);" >
         <div class="row">
-            <h5>Puerto Serial:</h5>
+            <h5 style="color: #F2B441">Puerto Serial:</h5>
             <div class="col-md-4">
-                <select class="form-select mb-2" aria-label="Default select example">
+                <select class="form-select mb-2" aria-label="Default select example" style="; background-color:#F2B441; border-color: #F27244">
                     <option value="COM0" selected>COM 0</option>
                     <option value="COM1">COM 1</option>
                     <option value="COM2">COM 2</option>
                     <option value="COM3">COM 3</option>
                 </select>
-                <button class="btn btn-warning col">Test</button>
+                <button class="btn btn-warning col" style="border-radius: 10;">Test</button>
                 <button class="btn btn-success col">Conectar</button>
             </div>
 
